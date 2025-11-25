41378e063e8887601efd73e789f791231c688e79
Andrés David Criollo <160885149+andresc1108@users.noreply.github.com>
2025-11-24 23:25:02 -0500

    Delete src/main directory

--- src/main/java/com/example/accesscontrol/repository/AlertaRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.accesscontrol.repository;
-
-import com.example.accesscontrol.model.Alerta;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface AlertaRepository extends MongoRepository<Alerta, String> {}
--- src/main/java/com/example/accesscontrol/repository/RegistroAccesoRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.accesscontrol.repository;
-
-import com.example.accesscontrol.model.RegistroAcceso;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface RegistroAccesoRepository extends MongoRepository<RegistroAcceso, String> {}
