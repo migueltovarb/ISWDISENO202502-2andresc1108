b1e350b2ff1703ed6709a9c30617daaf5803d040
Andrés David Criollo <160885149+andresc1108@users.noreply.github.com>
2025-11-24 23:23:56 -0500

    Delete ProyectoSpringBoot directory

--- ProyectoSpringBoot/vehiculos-api/src/main/java/com/example/vehiculos/repository/VehiculoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.vehiculos.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.example.vehiculos.model.Vehiculo;
-
-public interface VehiculoRepository extends MongoRepository<Vehiculo, String> {
-
-}
