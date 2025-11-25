ca0891650156d7a943cb32cb22045887d5bf85a3
Andrés David Criollo <160885149+andresc1108@users.noreply.github.com>
2025-11-24 23:21:57 -0500

    Delete ProyectoFinal/ProyectoSpringBoot directory

--- ProyectoFinal/ProyectoSpringBoot/src/main/java/com/example/accesscontrol/repository/CredencialRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.accesscontrol.repository;
-
-import com.example.accesscontrol.model.Credencial;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CredencialRepository extends JpaRepository<Credencial, String> {}
--- ProyectoFinal/ProyectoSpringBoot/src/main/java/com/example/accesscontrol/repository/PuertaRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.accesscontrol.repository;
-
-import com.example.accesscontrol.model.Puerta;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PuertaRepository extends JpaRepository<Puerta, String> {}
